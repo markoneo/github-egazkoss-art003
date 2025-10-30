575b10740f87bc76e07403cebfd51470e06ba1c0
markoneo <34494358+markoneo@users.noreply.github.com>
2025-10-30 18:56:37 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="py-8 bg-slate-950 border-t border-slate-800">
       <div className="max-w-7xl mx-auto px-6 text-center">
         <p className="text-slate-400">
-          © ARTComp {currentYear}
+          © ARTComp {currentYear} | Artcomp Slovenia | info@artcomp.net
         </p>
       </div>
     </footer>
