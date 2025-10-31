5641bb36e92e88985d39460fb9335f5a2662271e
markoneo <34494358+markoneo@users.noreply.github.com>
2025-10-31 05:55:45 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+      },
       colors: {
         'cream': {
           50: '#EDEDE9',
